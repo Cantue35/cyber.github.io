e8a19024be5fb0fc59c5ae80c7ef174d4275142d
Cantue35 <71815869+Cantue35@users.noreply.github.com>
2024-07-25 21:19:36 +0300

    Update

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,56 +1,53 @@
-var TxtType = function(el, toRotate, period) {
+class TxtType {
+    constructor(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
         this.period = parseInt(period, 10) || 2000;
         this.txt = '';
-        this.tick();
         this.isDeleting = false;
-    };
+        this.tick();
+    }
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+    tick() {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
 
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+        this.txt = this.isDeleting ? fullTxt.substring(0, this.txt.length - 1) : fullTxt.substring(0, this.txt.length + 1);
+        this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
+        let delta = 200 - Math.random() * 100;
         if (this.isDeleting) { delta /= 2; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
+            delta = this.period;
+            this.isDeleting = true;
         } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
         }
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
+        setTimeout(() => this.tick(), delta);
+    }
+}
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
+window.onload = function() {
+    const elements = document.getElementsByClassName('typewrite');
+    for (let i = 0; i < elements.length; i++) {
+        const toRotate = elements[i].getAttribute('data-type');
+        const period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            try {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            } catch (e) {
+                console.error('Error parsing data-type for typewrite effect:', e);
             }
         }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid rgba(255, 255, 255, 0.747)}";
-        document.body.appendChild(css);
-    };
+    }
+    // INJECT CSS
+    const css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid rgba(255, 255, 255, 0.747)}";
+    document.body.appendChild(css);
+};
